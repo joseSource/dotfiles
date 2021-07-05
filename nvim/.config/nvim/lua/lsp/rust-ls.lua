403954a590416ffa75ecef0b29b4079621c94c58
joseSource <miguelshane31@gmail.com>
2021-07-05 17:35:04 -0500

    nvim new setup, from me()

--- nvim/.config/nvim/lua/lsp/rust-ls.lua
+++
@@ -1,6 +0,0 @@
--- the rust-tools plugin will configure this automatically
---require'lspconfig'.rust_analyzer.setup{
---    cmd = {DATA_PATH .. "/lspinstall/rust/rust-analyzer"},
---    on_attach = require'lsp'.common_on_attach
---}
-
