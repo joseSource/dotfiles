f7e2eef0833f3a241ff207dafb6b65485dfaa7c3
joseSource <miguelshane31@gmail.com>
2021-07-06 11:13:39 -0500

    polybar changes*()

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-
-# Add this script to your wm startup file.
-
-DIR="$HOME/.config/polybar/docky"
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -10,5 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+polybar -rq music &
+polybar -rq tray &
+polybar -rq ws &
+
+
+echo "Polybar launched..."
